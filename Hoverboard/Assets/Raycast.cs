b1a06e0c03978babe6c5a3a6189c41ef365f550d
chimchim <b12nikli@SD20313.hs.local>
2014-04-02 13:19:27 +0200

    asd

---
+++ Hoverboard/Assets/Raycast.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Raycast : MonoBehaviour {
+	float distanceToGround;
+	// Use this for initialization
+	void Start () {
+		distanceToGround = 1;
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		Ray ray = new Ray (transform.position, -Vector3.up);
+		RaycastHit hit;
+		if (Physics.Raycast(ray, out hit, 100))
+			Debug.DrawLine(ray.origin, hit.point);
+
+	}
+}
