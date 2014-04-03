6591caccb0203309244230272a6ed219ddbf5ff0
Raful <rotor00a@hotmail.com>
2014-04-03 11:31:25 +0200

    Small fix to the joystick support

--- Hoverboard/Assets/Script/Movement.cs
+++ Hoverboard/Assets/Script/Movement.cs
@@ -13,17 +13,13 @@ public class Movement : MonoBehaviour {
 	// U>) or less than (<).
 	void Update () 
 	{
-        float movedX = Input.GetAxisRaw("Horizontal");
-        float movedY = Input.GetAxisRaw("Vertical");
-        //Debug.Log(movedX);
-		if (/*Input.GetKey (KeyCode.W) && */movedY > 0 && m_Velocity.z < 0.2f)
-            m_Velocity = m_Velocity + new Vector3(0, 0, 0.1f) * movedY;
+        //movementAxis reads the left joystick or wasd
+        Vector3 movementAxis = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical"));
 
-		//if (Input.GetKey (KeyCode.D))
-        m_Velocity = m_Velocity + new Vector3(0.1f, 0, 0) * movedX;
+        if (movementAxis.z > 0 && m_Velocity.z < 0.2f)
+            m_Velocity = m_Velocity + new Vector3(0, 0, 0.1f * movementAxis.z);
 
-		/*if (Input.GetKey (KeyCode.A))
-			m_Velocity = m_Velocity + new Vector3(-0.1f,0,0);*/
+        m_Velocity = m_Velocity + new Vector3(0.1f * movementAxis.x, 0, 0);
 
 		if (Input.GetButtonDown("Jump"))
 			m_Jumped = true;
@@ -33,6 +29,8 @@ public class Movement : MonoBehaviour {
 			m_Velocity.z -= 0.01f;
 		if(transform.position.y > 3)
 			transform.position -=  new Vector3(0,0.1f,0);
+
+        Debug.Log(m_Velocity);
 	}
 
 
