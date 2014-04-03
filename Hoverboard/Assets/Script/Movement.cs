1e1130deee40d65998b718945884c16311a2317c
Raful <rotor00a@hotmail.com>
2014-04-03 11:20:39 +0200

    Implemented support for xbox-controller

--- Hoverboard/Assets/Script/Movement.cs
+++ Hoverboard/Assets/Script/Movement.cs
@@ -13,14 +13,17 @@ public class Movement : MonoBehaviour {
 	// U>) or less than (<).
 	void Update () 
 	{
-		if (Input.GetKey (KeyCode.W) && m_Velocity.z < 0.2f)
-			m_Velocity = m_Velocity + new Vector3(0,0,0.1f);
+        float movedX = Input.GetAxisRaw("Horizontal");
+        float movedY = Input.GetAxisRaw("Vertical");
+        //Debug.Log(movedX);
+		if (/*Input.GetKey (KeyCode.W) && */movedY > 0 && m_Velocity.z < 0.2f)
+            m_Velocity = m_Velocity + new Vector3(0, 0, 0.1f) * movedY;
 
-		if (Input.GetKey (KeyCode.D))
-			m_Velocity = m_Velocity + new Vector3(0.1f,0,0);
+		//if (Input.GetKey (KeyCode.D))
+        m_Velocity = m_Velocity + new Vector3(0.1f, 0, 0) * movedX;
 
-		if (Input.GetKey (KeyCode.A))
-			m_Velocity = m_Velocity + new Vector3(-0.1f,0,0);
+		/*if (Input.GetKey (KeyCode.A))
+			m_Velocity = m_Velocity + new Vector3(-0.1f,0,0);*/
 
 		if (Input.GetButtonDown("Jump"))
 			m_Jumped = true;
