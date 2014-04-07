c91400919a920349c9c2d4684e4f3c566ca63e7c
spirant93 <spirit_93@live.se>
2014-04-07 17:12:24 +0200

    17:15

--- Hoverboard/Assets/Script/CameraMovement.cs
+++ Hoverboard/Assets/Script/CameraMovement.cs
@@ -5,20 +5,33 @@ public class CameraMovement: MonoBehaviour {
 
 	GameObject hoverboard; 
 	Vector3 oldPosition;
+	float xDifference = -10;
+	float yDifference = 3;
+	float wantedRotationAngle;
+	float currentRotationAngle;
+	Quaternion currentRotation;
 	// Use this for initialization
 	void Start () {
 		hoverboard = GameObject.Find("Hoverboard");
-		transform.position = new Vector3 (hoverboard.transform.position.x - 10, hoverboard.transform.position.y + 3, 
+		transform.position = new Vector3 (hoverboard.transform.position.x + xDifference, hoverboard.transform.position.y + yDifference, 
 		                                 hoverboard.transform.position.z);
-		oldPosition = transform.position;
+
 
 	}
 	
 	// Update is called once per frame
 	void Update () {
 
-		/*transform.position = new Vector3 (hoverboard.transform.position.x - 10, hoverboard.transform.position.y + 3, 
-		                                  hoverboard.transform.position.z);*/
+		wantedRotationAngle = hoverboard.transform.eulerAngles.y;
+		currentRotationAngle = transform.eulerAngles.y;
+		float newRotationAngle = currentRotationAngle - wantedRotationAngle ;
+		currentRotationAngle = Mathf.LerpAngle (currentRotationAngle, newRotationAngle, 3 * Time.deltaTime);
+		currentRotation = Quaternion.Euler (0, currentRotationAngle, 0);
+
+		transform.position -= currentRotation * Vector3.left;
+
+		transform.position = new Vector3 (hoverboard.transform.position.x + xDifference, hoverboard.transform.position.y + yDifference, 
+		                                  hoverboard.transform.position.z);
 
 
 		transform.LookAt (hoverboard.transform.position);
