3e889bd496532f466bb35c587c3d3e83d681e210
Raful <rotor00a@hotmail.com>
2014-04-08 16:59:49 +0200

    Support to detect what state (railing etc) the player is in.

---
+++ Hoverboard/Assets/Script/ColliderObject.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ColliderObject : MonoBehaviour {
+
+    [SerializeField]
+    string type;
+
+    public string m_type
+    {
+        get { return type; }
+    }
+
+    [HideInInspector]
+    public ArrayList m_states;
+
+	// Use this for initialization
+	void Start () 
+    {
+        m_states = new ArrayList();
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+        
+	}
+
+    void OnTriggerEnter(Collider col)
+    {
+        InteractObject script;
+        if ((script=col.gameObject.GetComponent<InteractObject>())!=null) //If the script exists in the collided object
+        {
+            m_states.Add(script.m_type);
+        }
+    }
+
+    void OnTriggerExit(Collider col)
+    {
+        InteractObject script;
+        if ((script = col.gameObject.GetComponent<InteractObject>()) != null) //If the script exists in the collided object
+        {
+            m_states.Remove(script.m_type);
+        }
+    }
+}
--- Hoverboard/Assets/Script/DetectState.cs
+++ Hoverboard/Assets/Script/DetectState.cs
@@ -3,26 +3,47 @@
  * The state is accessed with m_state, for use in other scripts.
  */
 
-
 using UnityEngine;
 using System.Collections;
 
 public class DetectState : MonoBehaviour {
 
     private string state = "Default"; //What state the player is in (grinding etc)
-
     public string m_state
     {
         get { return state; }
     }
 
+    private string[] availableStatesStrings =
+    {
+        "Rail"
+    };
+
+    ColliderObject[] colliderStates;
+
 	// Use this for initialization
-	void Start () {
-	
+	void Start () 
+    {
+        colliderStates = gameObject.GetComponentsInChildren<ColliderObject>();
 	}
 	
 	// Update is called once per frame
-	void Update () {
-	
+	void Update () 
+    {
+        foreach (ColliderObject colliderObject in colliderStates)
+        {
+            string tempStateString="default";
+
+            foreach (string stateString in availableStatesStrings)
+            {
+                if (colliderObject.m_states.Contains(stateString))
+                {
+                    tempStateString = stateString;
+                    break;
+                }
+            }
+
+            state = tempStateString;
+        }
 	}
 }
