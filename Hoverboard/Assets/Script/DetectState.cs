5cd301f61811c838f068bca9230bc68f29283075
Raful <rotor00a@hotmail.com>
2014-04-08 09:43:39 +0200

    Started on a new script

---
+++ Hoverboard/Assets/Script/DetectState.cs
@@ -0,0 +1,28 @@
+﻿/*
+ * This script detects what state (grinding etc) the player the player is in.
+ * The state is accessed with m_state, for use in other scripts.
+ */
+
+
+using UnityEngine;
+using System.Collections;
+
+public class DetectState : MonoBehaviour {
+
+    private string state = "Default"; //What state the player is in (grinding etc)
+
+    public string m_state
+    {
+        get { return state; }
+    }
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
