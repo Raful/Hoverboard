d85f019f1a2d076c2c5945f87b1774f7f9856a7c
swepw <a12ponwa@SD20302.hs.local>
2014-04-11 15:22:33 +0200

    tjohej, menyn har lite ljudinställningar..

--- Hoverboard/Assets/Script/Meny.cs
+++ Hoverboard/Assets/Script/Meny.cs
@@ -9,34 +9,87 @@ using System.Collections;
 
 
 
-public class Meny : MonoBehaviour {
 
+public class Meny : MonoBehaviour {
+	
 	public Texture2D Logotype;
 
 	//Bredd/höjd för menyknappar
-	public static int m_box_w = 350;
-	public static int m_box_h = 75;
+	public  static int m_box_w = 350;
+	public static int m_box_h = 100;
+
+	 
 
 	//Bredd/Höjd för Logotyp
 
 	public static int m_Logo_w = 256;
 	public static int m_Logo_h = 128;
 
+
+
+	int m_centered_x = Screen.width/2 - m_box_w/2;
+	int m_centered_y = Screen.height/2 - m_box_h/2; 	//vet inte om vi behöver använda denna, men det är bra att ha den definierad.
+	
+	int m_Logo_center_x = Screen.width/2 - m_Logo_w/2;
+	int m_Logo_center_y = Screen.height/2 - m_Logo_h/2;
+
+
 	bool master = true; //Håller koll på vilken meny vi är på
 
 	/*Här initieras de faktiska inställningarna*/
-	float m_fov = 90f;
-	//Camera.main.fieldOfView = m_fov;
+	public float m_fov = 90f;
+
+	public float volume = 100f;
+	public AudioSource music_audio;
+
+	
+
+
+	void Main_menu(){
+		if (GUI.Button (new Rect (m_centered_x, 148, m_box_w, m_box_h), "Start")) {
+			Application.LoadLevel ("Erik");
+		}
+		
+		if (GUI.Button (new Rect (m_centered_x, (148 + m_box_h), m_box_w, m_box_h), "Alternativ")) {
+			master = false;
+		}
+		if (GUI.Button (new Rect (m_centered_x, (148 + m_box_h * 2), m_box_w, m_box_h), "Avsluta")) {
+			Application.Quit ();
+		}
+	}
+	
 
 	void Options_menu()
 	{
+
+
 		int m_centered_x = Screen.width/2 - m_box_w/2;
-		GUI.Box (new Rect (20, m_Logo_h+20, Screen.width - 20, Screen.height - 20)," ");
-		if(GUI.Button (new Rect (m_centered_x, 148, m_box_w, m_box_h), "Tillbaka"))
+
+
+
+		int slide_x = Screen.width / 2 - 200;
+		int slide_y = 80;
+
+
+		m_fov = GUI.HorizontalSlider(new Rect (slide_x, m_Logo_h + slide_y, 400, 50),m_fov, 55, 120);
+		GUI.Label(new Rect (slide_x, m_Logo_h+ slide_y - 20, 200, 20), "Field of view");
+		GUI.Label(new Rect (Screen.width/2, m_Logo_h+ slide_y + 20, 22, 20), m_fov.ToString());
+
+		AudioListener.volume = GUI.HorizontalSlider(new Rect (slide_x, m_Logo_h + slide_y*2, 400, 50),AudioListener.volume, 0, 1);
+		GUI.Label (new Rect (slide_x, m_Logo_h + slide_y*2 - 20, 200, 20), "Master Audio");
+		GUI.Label (new Rect (Screen.width/2, m_Logo_h + slide_y*2 + 20, 22, 20), AudioListener.volume.ToString());
+
+		music_audio.volume = GUI.HorizontalSlider(new Rect (slide_x, m_Logo_h + slide_y*3, 400, 50),music_audio.volume, 0, 1);
+		GUI.Label (new Rect (slide_x, m_Logo_h + slide_y*3 - 20, 200, 20), "Music");
+		GUI.Label (new Rect (Screen.width/2, m_Logo_h + slide_y*3 + 20, 22, 20), music_audio.volume.ToString());
+
+
+
+
+		if(GUI.Button (new Rect (m_centered_x, m_Logo_h + slide_y*2 + 148, m_box_w, m_box_h), "Tillbaka"))
 		{
 			master = true;
 		}
-		m_fov = GUI.HorizontalSlider (new Rect (Screen.width/2-200, 148+m_box_h, 100, 50), m_fov, 55, 120);
 
 	}
 
@@ -49,29 +102,16 @@ public class Meny : MonoBehaviour {
 		 * Logo_center centrerar logotypen
 		 */
 
-		int m_centered_x = Screen.width/2 - m_box_w/2;
-		int m_centered_y = Screen.height/2 - m_box_h/2; 	//vet inte om vi behöver använda denna, men det är bra att ha den definierad.
 
-		int m_Logo_center_x = Screen.width/2 - m_Logo_w/2;
-		int m_Logo_center_y = Screen.height/2 - m_Logo_h/2;
 
 
 
 		GUI.DrawTexture (new Rect (m_Logo_center_x, 20, m_Logo_w, m_Logo_h), Logotype);
 
+
 		if (master) {
-						if (GUI.Button (new Rect (m_centered_x, 148, m_box_w, m_box_h), "Starta mig.")) {
-								Application.LoadLevel ("Erik");
-						}
-
-						if (GUI.Button (new Rect (m_centered_x, (148 + m_box_h), m_box_w, m_box_h), "Alternativ.")) {
-						master = false;
-						}
-
-						if (GUI.Button (new Rect (m_centered_x, (148 + m_box_h * 2), m_box_w, m_box_h), "Avsluta.")) {
-								Application.Quit ();
-						}
-				} 
+			Main_menu();
+		}
 		else 
 		{
 			Options_menu();
