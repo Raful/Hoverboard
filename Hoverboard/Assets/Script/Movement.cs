48fb21e90ff44501ec46debc856682b022f56ed4
chimchim <b12nikli@SD20315.hs.local>
2014-04-14 11:18:56 +0200

    HoverBoard 2.0

--- Hoverboard/Assets/Script/Movement.cs
+++ Hoverboard/Assets/Script/Movement.cs
@@ -1,4 +1,5 @@
 ﻿using UnityEngine;
+using UnityEngine;
 using System.Collections;
 
 /*
@@ -10,68 +11,58 @@ using System.Collections;
  */
 public class Movement : MonoBehaviour {
 	
-	Ray m_rayDown;
-	RaycastHit hitDown;
-	Raycast[] rayarray;
-	
-	public float m_Acceleration;
-	public float m_AngleSpeed;
-	public float m_RayLength;
 	public float m_Speed;
-	private Quaternion goToRotation;
-	private Vector3 newDirection;
-	private Vector3 rayDirDelta;
-	
-	private bool m_rotate;
-	
-
-	private float frontAngle;
-	public float forwardOn;
-	private int gravityOn;
-	public int angleOn;
-	
-	
+	public float m_Acceleration;
+	public float m_rotationSpeed;
+	private bool accelerate;
 	void Start (){
 		
-		rayarray = GetComponentsInChildren<Raycast>();
-		angleOn = 1;
-		forwardOn = 0;
-		gravityOn = 0;
-		m_rayDown.origin = transform.position;
 		
 	}
 	
-	// TODO, Spara velocity, fixa gravity n push, vinklarna i luften
+	
 	void Update () 
 	{
-		if(m_rotate)
+		RaycastHit hit;
+		if(Physics.Raycast(transform.position, -transform.up, out hit, 10))
 		{
-			if(Input.GetKey(KeyCode.W) && m_Speed <1.0f )
+			accelerate = true;
+			transform.rotation = Quaternion.Lerp(transform.rotation,Quaternion.LookRotation(Vector3.Cross(transform.right, hit.normal)),Time.deltaTime*m_rotationSpeed);
+		}
+		else
+		{
+			accelerate = false;
+		}
+		if(accelerate)
+		{
+			if(Input.GetKey(KeyCode.W) && m_Speed <100.0f )
 			{
-				m_Speed += m_Acceleration/ 1000;
+				m_Speed += m_Acceleration/ 10;
 			}
 			if(Input.GetKey(KeyCode.S))
 			{
-				m_Speed -= m_Acceleration/ 1000;
-			}
-			
-			if(Input.GetKey(KeyCode.A))
-			{
-				transform.Rotate(0,-1f,0);
-			}
-			if(Input.GetKey(KeyCode.D))
-			{
-				transform.Rotate(0,1f,0);
+				m_Speed -= m_Acceleration/ 10;
 			}
 		}
-		else{
+		if(Input.GetKey(KeyCode.A))
+		{
+			transform.Rotate(0,-1f,0);
+		}
+		if(Input.GetKey(KeyCode.D))
+		{
+			transform.Rotate(0,1f,0);
+		}
+
+
+		/*	else
+		{
 			if(Input.GetKey(KeyCode.W))
 			{
-				transform.Rotate(2f,0,0);
+				transform.Rotate(1f,0,0);
 			}
 			if(Input.GetKey(KeyCode.S))
 			{
-				transform.Rotate(-2f,0,0);
+				transform.Rotate(-1f,0,0);
 			}
 			
 			if(Input.GetKey(KeyCode.A))
@@ -82,53 +73,11 @@ public class Movement : MonoBehaviour {
 			{
 				transform.Rotate(0,1f,0);
 			}
-		}
+		}*/
+
+			transform.position += transform.forward.normalized*m_Speed*Time.deltaTime;
 		
-		m_rayDown.direction = -transform.up;
-		m_rayDown.origin = transform.position;
 		
-		// Down
-		if (Physics.Raycast (m_rayDown, out hitDown, m_RayLength)) 
-		{	
-			Debug.DrawLine (m_rayDown.origin, hitDown.point);
-			m_rotate = true;
-		}
-		else
-		{
-			m_rotate = false;
-			rigidbody.velocity = new Vector3(0,-5,0);
-			hitDown.distance = 4;
-		}
-		
-		if (hitDown.distance < 2)
-		{
-			
-			newDirection = Vector3.Cross(transform.right, hitDown.normal);
-			angleOn = 0;
-			forwardOn = 1;
-			rigidbody.AddForce (hitDown.normal * 10);
-		} 
-		//Vinkelhastighet
-		else
-		{
-			angleOn = 1;
-			forwardOn = 0;
-		}
-		
-		if(m_rotate)
-		{
-			goToRotation = Quaternion.Lerp(transform.rotation,Quaternion.LookRotation(Vector3.Cross(transform.right, hitDown.normal), hitDown.normal),Time.deltaTime*m_AngleSpeed);
-			transform.rotation = goToRotation;
-		}
-		
-		if(m_Speed > 0.01f)
-			m_Speed -= 0.001f;
-		
-		frontAngle =  (rayarray[0].m_Length+rayarray[1].m_Length+rayarray[2].m_Length+rayarray[3].m_Length);
-		if (frontAngle > 12.7f)
-			frontAngle = 12.7f;
-		
-		transform.position += (angleOn*transform.forward + newDirection*forwardOn).normalized * m_Speed*(frontAngle/(12.7f));
 	}
 	
 }
