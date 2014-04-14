65669e65d7c9cc728bb245404800c426e6322e45
spirant93 <spirit_93@live.se>
2014-04-14 10:06:04 +0200

    10:10

--- Hoverboard/Assets/Script/CameraMec.cs
+++ Hoverboard/Assets/Script/CameraMec.cs
@@ -12,12 +12,18 @@ public class CameraMec : MonoBehaviour {
 
 	void Update() {
 
+		//calculating how much the camera should rotate in y- and x-axis relative to the Hoverboard
 		float yAngle = Mathf.SmoothDampAngle(transform.eulerAngles.y, target.eulerAngles.y, ref yVelocity, smooth);
 		float xAngle = Mathf.SmoothDampAngle(transform.eulerAngles.x, target.eulerAngles.x, ref xVelocity, smooth);
-		float zAngle = Mathf.SmoothDampAngle(transform.eulerAngles.z, target.eulerAngles.z, ref zVelocity, smooth);
+
+		//creating new position relative to the hoverboard. 
 		Vector3 position = target.position + new Vector3(0,1,0);
 		Vector3 newPos = position;
+
+		//Then adding the angles *(-distance) there distance is the distance between the camera and the hoverboard in each axis.
 		 newPos +=  Quaternion.Euler(xAngle, yAngle, 0) * new Vector3(0, 0, -distance);
+
+		//give camera the position "newPos"
 		transform.position = newPos;
 		transform.LookAt(position);
 
