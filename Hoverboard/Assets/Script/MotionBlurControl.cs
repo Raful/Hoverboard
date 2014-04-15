27ade052361a99c1074ff6fb753c14cc535bdccf
Nygaard32 <easen53@gmail.com>
2014-04-15 15:24:41 +0200

    Removed more files that aren't used, and more changes have been made to BlurControlScript

--- Hoverboard/Assets/Script/MotionBlurControl.cs
+++ Hoverboard/Assets/Script/MotionBlurControl.cs
@@ -5,17 +5,28 @@ public class MotionBlurControl : MonoBehaviour {
 
 	MotionBlur Areablur;
 	Movement StartBlur;
+	public float m_SpeedThreshold, m_IncAmount, m_DecAmount, m_BlurLimit;
+	private float Zero = 0f;
 
 	// Use this for initialization
 	void Start () {
+
 		Areablur = gameObject.GetComponent<MotionBlur>();
 		StartBlur = GameObject.Find("Hoverboard 2.0").GetComponent<Movement>();
+		Areablur.blurAmount = Zero;
+
 	}
 	
 	// Update is called once per frame
 	void Update () 
 	{
-
+		if(StartBlur.m_Speed > m_SpeedThreshold && Areablur.blurAmount < m_BlurLimit)
+		{
+			Areablur.blurAmount += m_IncAmount;
+		}
+		else
+		{
+			Areablur.blurAmount -= m_DecAmount;
+		}
 	}
-	
 }
