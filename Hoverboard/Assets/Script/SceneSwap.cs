deae176e2d39bb061d0b8003d8754966836f512b
Stibbins <john.spjuth@gmail.com>
2014-04-16 15:53:23 +0200

    Ny build med keypad 1-3 för att ändra scener och esc för att avsluta

---
+++ Hoverboard/Assets/Script/SceneSwap.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SceneSwap : MonoBehaviour {
+
+	void Awake() {
+		DontDestroyOnLoad(transform.gameObject);
+	}
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+		if (Input.GetKey(KeyCode.Keypad1)){
+		
+			Application.LoadLevel(0);
+
+		}
+
+		
+		if (Input.GetKey(KeyCode.Keypad2)){
+			
+			Application.LoadLevel(1);
+			
+		}
+
+		if (Input.GetKey(KeyCode.Keypad3)){
+			
+			Application.LoadLevel(2);
+			
+		}
+
+		if (Input.GetKey(KeyCode.Escape)){
+
+			Application.Quit();
+
+		}
+
+	}
+}
