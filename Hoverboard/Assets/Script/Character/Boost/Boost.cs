9ea6a289f435eecb7ccbcaf497d9c23b18dee8ca
Raful <rotor00a@hotmail.com>
2014-04-18 09:37:15 +0200

    Added boost scripts and move folders

---
+++ Hoverboard/Assets/Script/Character/Boost/Boost.cs
@@ -0,0 +1,42 @@
+﻿/*
+ * Created by: Robbin
+ * Modified by: 
+ * 
+ * Description:
+ * Increases the acceleration of the character by pressing a button.
+ */
+
+using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(EnergyPool))]
+
+public class Boost : MonoBehaviour {
+
+    EnergyPool energyScript;
+
+    [SerializeField]
+    float energyDrainSpeed=1;
+    [SerializeField]
+    float pauseDuration=1;
+
+	// Use this for initialization
+	void Start () {
+        energyScript = gameObject.GetComponent<EnergyPool>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetButton("Boost"))
+        {
+            UseBoost();
+        }
+	}
+
+    void UseBoost()
+    {
+        energyScript.Decrease(energyDrainSpeed * Time.deltaTime);
+
+        energyScript.Pause(pauseDuration);
+    }
+}
