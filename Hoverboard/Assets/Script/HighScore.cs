3336bef132f1045e190b77e12be8e970cbcff252
Raful <rotor00a@hotmail.com>
2014-04-22 11:04:33 +0200

    added highscore script

---
+++ Hoverboard/Assets/Script/HighScore.cs
@@ -0,0 +1,31 @@
+﻿/*
+ * Created by: Robbin
+ * Modified by: 
+ * 
+ * Description:
+ * 
+ */
+
+using UnityEngine;
+using System.Collections;
+
+public class HighScore : MonoBehaviour {
+
+    string userName="Not defined username";
+
+	void Start ()
+    {
+#if UNITY_EDITOR
+        InitPC();
+#endif
+    }
+	
+	void Update () {
+        Debug.Log(userName);
+	}
+
+    void InitPC()
+    {
+        userName = "UserOnPC";
+    }
+}
