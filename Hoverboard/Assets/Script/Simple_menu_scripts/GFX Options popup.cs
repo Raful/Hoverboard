a38d3e03b630f8cd428984affd1cd12c4f4b65ec
swepw <hastati_2000@hotmail.com>
2014-04-24 16:39:28 +0200

    Är inte helt säker på att jag addade innan

---
+++ Hoverboard/Assets/Script/Simple_menu_scripts/GFX Options popup.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GFX_Options_popup : MonoBehaviour {
+	public UIPopupList popup;
+	public string myItem;
+	
+	void Update()
+	{
+		if (popup.selection == "Low") 
+		{
+			QualitySettings.SetQualityLevel(1, true);
+		}
+	}
+}
