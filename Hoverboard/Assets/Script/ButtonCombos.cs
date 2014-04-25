39bbdc91d1b42d42cf8d882a6e564e0108599673
spirant93 <a12andsu@SD20310.hs.local>
2014-04-25 10:22:30 +0200

    25-04-14   10:22

--- Hoverboard/Assets/Script/ButtonCombos.cs
+++ Hoverboard/Assets/Script/ButtonCombos.cs
@@ -13,14 +13,14 @@ using System.Collections.Generic;
 
 public class ButtonCombos : MonoBehaviour {
 
-	public string m_Keys = "";
-	private float lastTimeSincePressed = 0;
-	public List<string> combos = new List<string>();
-	public List<string> tricks = new List<string>();
+	public string m_Keys = "";   							//the string of the buttons that is pressed in a time interval	
+	private float lastTimeSincePressed = 0;			
+	public List<string> combos = new List<string>();		//These two lists contains the tricks and combinations that is possible.
+	public List<string> tricks = new List<string>();		// The elemnts in the lists will be put in the Dictionary 'comboList' because it is easier to use.
 	private Dictionary<string, string> comboList = new Dictionary<string, string>();
 
-	public float comboTime = 1f;
-	public float afterThisTimeTheTrickStarts = 0.5f;
+	public float comboTime = 0.2f;
+	public float afterThisTimeTheTrickStarts = 0.35f;
 	// Use this for initialization
 	void Start () {
 		if(combos.Count == tricks.Count)
@@ -44,12 +44,13 @@ public class ButtonCombos : MonoBehaviour {
 
 		Debug.Log(m_Keys );
 
+		// if a combination exist in the dictionary, and if the time value is more than the value in the variable 'afterThisTimeTheTrickStarts' then the trick starts 
 		if(comboList.ContainsKey(m_Keys) && (Time.time - lastTimeSincePressed) > afterThisTimeTheTrickStarts)
 		{
 			string trick = "";
-			if(comboList.TryGetValue(m_Keys, out trick))
-			{
-				switch(trick)
+			if(comboList.TryGetValue(m_Keys, out trick)) 	//Here comboList retrieves the trick name that has the same position as m_Keys and put it in 'trick'.
+			{												
+				switch(trick)								//Here the trick with the name that is the same as the string in 'trick will be called.
 				{
 				case "":
 					break;
@@ -64,6 +65,7 @@ public class ButtonCombos : MonoBehaviour {
 	}
 
 
+	//this function checks if has taken too much time since the last key was pressed. if the time isnt too much then m_Keys will add the string 'key' else m_Keys resets and get the string 'key' 
 	private void CheckKey(string key)
 	{
 		if((Time.time - lastTimeSincePressed) <= comboTime)
