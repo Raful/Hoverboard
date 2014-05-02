77fd758e725adc07ad0d4dcdd4ecdb2cc7765454
chimchim <b12nikli@SD20310.hs.local>
2014-05-02 12:06:58 +0200

    Grind

--- Hoverboard/Assets/Script/Character/DetectState/DetectState.cs
+++ Hoverboard/Assets/Script/Character/DetectState/DetectState.cs
@@ -68,7 +68,6 @@ public class DetectState : MonoBehaviour {
 	void Update () 
     {
         gatherColliders();
-		Debug.Log("CurrentState: " + currentKeyState);
         setState();
 		updateKeyState (currentKeyState).update();
         //Clear collidersFound at each frame, to keep it updated
