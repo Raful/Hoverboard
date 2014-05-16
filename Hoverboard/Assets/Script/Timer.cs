263b451ce9692549643d17f7e8dc49eaff8b7d88
Felix Mossberg <hakurou46@gmail.com>
2014-05-16 10:49:56 +0200

    poke

--- Hoverboard/Assets/Script/Timer.cs
+++ Hoverboard/Assets/Script/Timer.cs
@@ -14,7 +14,7 @@ public class Timer : MonoBehaviour {
 	float timeMinutes, timeSeconds, timeMilli;
 	float raceTime, leaveTime, finishTime;
 	short noResetTimer = 0;
-	
+	string text;
     public float m_finishTime
     {
         get { return finishTime; }
@@ -26,7 +26,27 @@ public class Timer : MonoBehaviour {
 		SetRaceTimer ();
 		SetMinSecMil ();
 
-		guiText.text = timeMinutes.ToString () + " : " + timeSeconds.ToString () + " : " + timeMilli.ToString ();
+		text="";
+
+		//minutes
+		if (timeMinutes<10){
+			text+="0";
+		} text+=timeMinutes.ToString () + ":";
+
+		//seconds
+		if (timeSeconds<10){
+			text+="0";
+		} text+=timeSeconds.ToString () + ".";
+
+		//milliseconds
+		if (timeMilli<100){
+			if (timeMilli<10){
+				text+="0";
+			} text+="0";
+		} text+=timeMilli.ToString ();
+
+		//render
+		guiText.text = text;
 	
 	}
 
