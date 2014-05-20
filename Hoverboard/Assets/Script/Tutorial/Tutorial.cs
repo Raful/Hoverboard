4fcc65d065485209215e305ff86308a2839cab9f
Stibbins <john.spjuth@gmail.com>
2014-05-20 14:34:55 +0200

    new sounds to the the tutorial

---
+++ Hoverboard/Assets/Script/Tutorial/Tutorial.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+using FMOD.Studio;
+
+public class Tutorial : MonoBehaviour {
+
+
+	[SerializeField]
+	private int boxNumber;
+	[SerializeField]
+	private GUITexture instructionsDisplay;
+	[SerializeField]
+	private FMODAsset soundFile;
+	[SerializeField]
+	private Transform camera;
+	
+	
+	private Transform playAt;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		playAt = camera;
+	}
+	
+	void OnTriggerEnter (Collider col)
+	{
+		if (col.tag == "TutorialBox")
+			 FMOD_StudioSystem.instance.PlayOneShot(soundFile, playAt.position);
+	}
+}
