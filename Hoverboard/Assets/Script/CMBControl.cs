8583713b155bd7d15a253e0fb3c1c5ad312d34a8
Nygaard32 <b12erias@SD20315.hs.local>
2014-05-20 17:12:23 +0200

    There is one shader working for blur, but radial blur shaders return an error so it doesn't work

---
+++ Hoverboard/Assets/Script/CMBControl.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CMBControl : MonoBehaviour {
+
+	[SerializeField]
+	private CameraMotionBlur CamBlur;
+	[SerializeField]
+	private Movement m_MovementReference;
+
+	[SerializeField]
+	private float incAmount, decAmount, speedThreshold;
+
+	// Use this for initialization
+	void Awake () 
+	{
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if(m_MovementReference.m_getVelocity.magnitude >= speedThreshold)
+		{
+			CamBlur.maxVelocity += incAmount;
+		}
+		else
+		{
+			CamBlur.maxVelocity -= decAmount;
+		}
+
+	}
+}
