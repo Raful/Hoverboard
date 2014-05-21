edee89a47fae38970380d289d6f07c544cf0f6dc
chimchim <b12nikli@SD20310.hs.local>
2014-05-21 16:17:45 +0200

    particles on BOARD!

--- Hoverboard/Assets/FollowLogic.cs
+++ Hoverboard/Assets/FollowLogic.cs
@@ -5,13 +5,16 @@ public class FollowLogic : MonoBehaviour {
 	public GameObject logicBoard;
 	private Movement movement;
 	private DetectState detectState;
-	public ParticleSystem railParticles;
+	private ParticleSystem railParticles;
+	private ParticleSystem boostParticles;
 
 	private bool floatup;
 	private Vector3 hover;
 	// Use this for initialization
 	void Start () 
 	{
+		boostParticles = transform.Find ("Main_ctrl/Spin_Ctrl/Hoverboard_01/Boost").GetComponent<ParticleSystem> ();
+		railParticles = transform.Find ("Main_ctrl/Spin_Ctrl/Hoverboard_01/Sparks").GetComponent<ParticleSystem> ();
 		hover = new Vector3 (0, 0, 0);
 		detectState = logicBoard.GetComponent<DetectState> ();
 		movement = logicBoard.GetComponent<Movement> ();
@@ -25,15 +28,21 @@ public class FollowLogic : MonoBehaviour {
 		if(detectState.getKeyState.Equals("Rail"))
 		{
 			transform.eulerAngles = new Vector3(transform.eulerAngles.x, transform.eulerAngles.y, logicBoard.transform.eulerAngles.z);
-		}
-		if(detectState.getKeyState == "Rail")
-		{
 			railParticles.maxParticles = (int)(movement.m_getVelocity.magnitude*1.4f);
 			railParticles.enableEmission = true;
 		}
 		else
 		{ 
 			railParticles.enableEmission = false;
+		}
+
+		if (movement.GetComponent<Boost> ().m_isBoosting) 
+		{
+			boostParticles.enableEmission = true;
+		}
+		else 
+		{
+			boostParticles.enableEmission = false;
 		}
 
 		transform.position = logicBoard.transform.position;
