abbb7c4c364850ad4ef1033e987ad4343170a882
Stibbins <john.spjuth@gmail.com>
2014-05-25 11:11:18 +0200

    texture display for tutorial up and running

---
+++ Hoverboard/Assets/Script/GUITextureDisplay.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+
+public class GUITextureDisplay : MonoBehaviour {
+
+
+	//private GUITexture
+	private Rect pixelInsetRect;
+	// Use this for initialization
+	void Start () 
+	{
+		
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		
+		//guiTexture.pixelInset.center = new Vector2(Screen.width/2,Screen.height/2);
+		//guiTexture.pixelInset = guiTexture.pixelInset.center;
+	}
+	
+	void OnGUI()
+	{
+		//transform.position = new Vector3(0.5f, 0.5f, 0);
+		//transform.localScale = new Vector3(0.5f, 0.5f, 0);
+		transform.localScale = Vector3.zero;
+		//guiTexture.texture.width = 824;
+		//guiTexture.texture.height = 248;
+		//pixelInsetRect.width = (Screen.width - guiTexture.texture.width/2)/4;
+		//pixelInsetRect.height = (Screen.height - guiTexture.texture.height/2)/4;
+		pixelInsetRect.width = guiTexture.texture.width;
+		pixelInsetRect.height = guiTexture.texture.height;
+		pixelInsetRect.y = (Screen.height/2 - guiTexture.texture.height/2);
+		pixelInsetRect.x = (Screen.width/2 - guiTexture.texture.width/2);
+		pixelInsetRect.center = new Vector2(Screen.width/2, Screen.height/2);
+		guiTexture.pixelInset = pixelInsetRect;
+	}
+}
