ebdac05259c7e8a7ff4d4c3ad78f30aeaab8e6b0
spirant93 <spirit_93@live.se>
2014-05-26 16:26:04 +0200

    26-05-2014  16:26

--- Hoverboard/Assets/Script/CameraMec2.cs
+++ Hoverboard/Assets/Script/CameraMec2.cs
@@ -10,52 +10,51 @@ using System.Collections;
  */
 public class CameraMec2 : MonoBehaviour {
 	
-	public Transform target;
-	private FollowLogic follow;
-	//private DetectState state;
-	public float m_DefaultDistance;
-	public float distance;   // The distance in the x-z plane to the target
-	private float decidedDefaultDistance;
-	private float jumpDistance;
-	public float groundHeight;   // the height we want the camera to be above the target when the target is in ground state
-	public float airHeight;      // the height we want the camera to be above the target when the target is in air state
-	private float height;					//current height the camera is above the target
-	public float upRampHeight;
-	public float downRampHeight;
-	private float defaultHeight;
-	public float heightDamping;	//how smoothly the camera follows the target in height
-	private float rotationDamping; //how smoothly the camera follows the target in rotation
-	public float defaultHeigtDamping;
+	public Transform target; // the target that the camera is looking at. THE TARGET SHOULD BE Model.
+	private FollowLogic follow; // the camera fetches values here.
 
-	public float defaultRotationDamping;
-	private float wantedRotationAngle;
-	private float wantedHeight;
-	private float currentRotationAngle;
-	private float currentX;
-	private float wantedX;
-	private float oldWantedHeight;
-	private float currentHeight;
-	private Vector3 targetedPos;
-	private Quaternion currentRotation;
+	private float distance;   // The distance in the x-z plane to the target.
+	public float m_DefaultDistance; // the default value for distance.
 
-	private float yVelocity = 0;
-	private float xVelocity = 0;
-	public float angleToChange; 
+
+
+	public float groundHeight;   // the height we want the camera to be above the target when the target is in ground state.
+	public float airHeight;      // the height we want the camera to be above the target when the target is in air state.
+	private float height;		//current height the camera is above the target
+	public float upRampHeight;  // the height we want the camera to be above the target when the target is going up a ramp..
+	public float downRampHeight; // the height we want the camera to be above the target when the target is going down a ramp.
+
+	private float heightDamping;	//how smoothly the camera follows the target in height.
+	private float rotationDamping; //how smoothly the camera follows the target in rotation.
+	public float defaultHeigtDamping; //default value for heightDamping.
+
+	public float defaultRotationDamping; //default value for rotationDamping.
+	private float wantedRotationAngle;  //the angles that the camera wants to be in.
+	private float wantedHeight;			//the height the camera want to have above the target.
+	private float currentRotationAngle;	//the angles the camera currently is having.
+
+
+	private float currentHeight; 	// the height above the target the camera has.
+
+	private Quaternion currentRotation;  // this becomes the angles that the camera gets.
+
+	private float yVelocity = 0;	//dont know what this is good for but need it in smoothDampAngle
+
+	public float angleToChange;		//if the targets x-angle is higher than this value the camera change state. 
 	
 
 	
 	void Start()
 	{
-		targetedPos = target.position;
+
 		follow = target.GetComponent<FollowLogic> ();
-		//state = target.GetComponent<DetectState> ();
+
 		height = groundHeight;
 		distance = m_DefaultDistance;
-		oldWantedHeight = 0;
-		decidedDefaultDistance = m_DefaultDistance;
-		jumpDistance = decidedDefaultDistance + 5;
+
+		heightDamping = defaultHeigtDamping;
+
 	
-		defaultHeight = height;
 
 
 	}
@@ -65,8 +64,8 @@ public class CameraMec2 : MonoBehaviour {
 		if (!target)
 			return;
 		
-		if(follow.getKeyState().Equals("Air"))
-		{
+		if(follow.getKeyState().Equals("Air"))	//if the target is in Air-state the cameras height change to 'airHeight' 
+		{										//and rotationamping becomes the value from defaultRotationDamping	
 			//heightDamping = 100;
 			if(height < airHeight-0.1f)
 				height += 0.2f;
@@ -77,8 +76,8 @@ public class CameraMec2 : MonoBehaviour {
 
 			rotationDamping = defaultRotationDamping;
 		}
-		else if(target.eulerAngles.x > angleToChange && target.eulerAngles.x < 150)
-		{
+		else if(target.eulerAngles.x > angleToChange && target.eulerAngles.x < 150) // if the target is going downwards on a ramp the height change to 'downRampHeight'
+		{																		    //and rotationDamping becomes the value from defaultRotationDamping	
 
 			if(height < downRampHeight)
 				height += 0.4f;
@@ -86,15 +85,15 @@ public class CameraMec2 : MonoBehaviour {
 				height = downRampHeight;
 			rotationDamping = defaultRotationDamping;
 		}
-		else if(follow.getXAngleForLogicBoard() > angleToChange)
-		{
+		else if(follow.getXAngleForLogicBoard() > angleToChange) //if the target is going downwards in a ramp the height change to 'downRampHeight'
+		{														 //and rotationDamping becomes the value from defaultRotationDamping	
 			if(height > upRampHeight)
 				height -= 0.5f;
 			else
 				height = upRampHeight;
 			rotationDamping = defaultRotationDamping;
 		}
-		else
+		else  // else it change to ground state and the height change to 'groundHeight'
 		{
 			//heightDamping = defaultHeigtDamping;
 			if(height > groundHeight +0.1f)
@@ -106,34 +105,31 @@ public class CameraMec2 : MonoBehaviour {
 		}
 
 	
-		if(target.eulerAngles.x < angleToChange && !follow.getKeyState().Equals("Air"))
-		{
+		if(target.eulerAngles.x < angleToChange && !follow.getKeyState().Equals("Air")) // if the camera is not on a ramp and not in the air
+		{																				//rotationDamping gets the value from DefalutRotationDamping
 			//heightDamping = defaultHeigtDamping;
 			rotationDamping = defaultRotationDamping;
 
 		}
-		else if(target.eulerAngles.x > angleToChange && !follow.getKeyState().Equals("Air"))
-		{
-			//heightDamping = 100;
+		else if(target.eulerAngles.x > angleToChange && !follow.getKeyState().Equals("Air")) // if the camera is on a ramp and not in the air
+		{																					 // rotationDamping's value becomes 0
+			//heightDamping = 100;	
 			rotationDamping = 0;
 		
 		}
-		else if(target.eulerAngles.x > angleToChange)
-		{
+		else if(target.eulerAngles.x > angleToChange) //if camera's x-angle is higher than angleToChange then and is in the air 
+		{											  //rotationDamping becomes the value from defaultRotationDamping
 
 			rotationDamping = defaultRotationDamping;
 		}
 		else
 		{
 			//heightDamping = 100;
-			//Debug.Log (heightDamping);
 			rotationDamping = 0;
-			//Debug.Log ("3");
+
 		}
-		// Calculate the current rotation angles
-		float y = targetedPos.y;
-		targetedPos = target.position;
-		targetedPos.y = y;
+
+	
 		wantedRotationAngle = target.eulerAngles.y;
 
 		
@@ -141,13 +137,12 @@ public class CameraMec2 : MonoBehaviour {
 		
 		currentRotationAngle = transform.eulerAngles.y;
 		currentHeight = transform.position.y;
-		currentX = transform.eulerAngles.x;
-		wantedX = target.eulerAngles.x;
+	
 		
 		// Damp the rotation around the y-axis
 		currentRotationAngle = Mathf.SmoothDampAngle (currentRotationAngle, wantedRotationAngle,ref yVelocity ,rotationDamping );
 		
-		currentX = Mathf.SmoothDampAngle (currentX, wantedX, ref xVelocity, rotationDamping);
+
 		
 
 			
@@ -156,15 +151,13 @@ public class CameraMec2 : MonoBehaviour {
 		currentHeight = Mathf.Lerp (currentHeight, wantedHeight, heightDamping * Time.deltaTime);
 	
 		// Convert the angle into a rotation
-		
-		
-		
 		currentRotation = Quaternion.Euler (0, currentRotationAngle, 0);
-		
+
+
 		// Set the position of the camera on the x-z plane to:
-		// distance meters behind the target
 		transform.position = target.position;
-		
+
+		//then subtract the rotation*distance from the position
 		transform.position -= currentRotation * Vector3.forward * distance;
 		
 		// Set the height of the camera
